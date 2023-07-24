e7601453c66069f9406e36cadbd0324a951b5a09
leesuman <99081816+leesuman@users.noreply.github.com>
2023-07-24 23:17:08 +0900

    a

---
+++ copySocar/copy.js/index.js
@@ -0,0 +1,37 @@
+
+const header = document.querySelector('header');
+let 이전좌표 = 0;
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 100) {
+        header.classList.add('scrolled')
+    } else {
+        header.classList.remove('scrolled');
+    }
+
+    //scroll 이벤트리스너일때
+    //스크롤을 올렸는지 내렸는지 파악하려면
+    //변수를 하나 선언 해야 한다.
+    if (이전좌표 < window.scrollY) {
+        header.classList.add('hide');
+    } else {
+        header.classList.remove('hide');
+    }
+
+    이전좌표 = window.scrollY;
+})
+
+const scrollContent = document.querySelectorAll('.advantage > *')
+makeScrollContents(scrollContent);
+
+makeCountUp(document.querySelector('.count-up'), 8985351, 4000, 24);
+
+
+const incline = document.querySelector('.incline');
+
+incline.style.left = '50%';
+
+window.addEventListener('scroll', () => {
+    if (incline.getBoundingClientRect().top - window.innerHeight < 0) {
+        incline.style.left = '0%';
+    }
+})
